873a67f27aed20973fafc995bf4d867bf0582954
Ritik Kanotra <ritikkanotra@gmail.com>
2021-07-09 15:17:23 +0530

    Add splash screen UI

---
+++ app/src/main/java/com/agastya/app/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.agastya.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+    }
+}
